3c48d219754f23ead79f1497a196d3d4bb1f74ec
Christian Salway <christian@XiansMacMini1.local>
2025-07-21 10:11:48 +0100

    removed IMDSv2 requirement

--- aws/standards/foundation/region/ec2.tf
+++ aws/standards/foundation/region/ec2.tf
@@ -1,6 +1,6 @@
 # Prowler - IMDSv2 should be enabled by default for EC2 instances
 
 resource "aws_ec2_instance_metadata_defaults" "enforce-imdsv2" {
-  http_tokens                 = "required"
-  http_put_response_hop_limit = 1
+  http_tokens                 = "optional"
+  # http_put_response_hop_limit = 1
 }
